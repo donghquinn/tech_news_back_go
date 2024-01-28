65d8448bfe2de2e2b07f55fd478b738f44abd2d3
donghquinn <ehdgus1524@gmail.com>
2024-01-28 10:22:08 +0900

    wip: CORS middlewares

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -23,21 +22,8 @@ func main(){
 	gin.SetMode(mode)
 
 	router.Use(middlewares.GlobalMiddleware())
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONS")
-			c.AbortWithStatus(400)
-		} else {
-			c.Next()
-		}
-	})
+	router.Use(middlewares.CorsMiddlewares())
+	
 	server := &http.Server{
 		Addr: port,
 		Handler: router,
---
+++ middlewares/cors.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddlewares() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(204)
+
+			return
+		}
+		c.Next()
+	}
+}
