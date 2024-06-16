16d00cfe8b3df0dca1f84f1870ca87ab3003cbfa
donghquinn <ehdgus1524@gmail.com>
2024-06-17 01:06:14 +0900

    wip: response

--- types/news/hacker.dto.go
+++ types/news/hacker.dto.go
@@ -6,7 +6,7 @@ import "github.com/dongquinn/tech_news_back_go/types"
 type HackerNewsResponseType struct {
 	Result bool `json:"result"`
 	Code string `json:"code"`
-	News []types.HackerNewsResponse
+	News []types.HackerNewsResponse	`json:"news"`
 }
 
 type HackerNewsErrorResponseType struct {
