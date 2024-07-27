f63f2d9841b39ea63fac8229b46b167c6b8ce63d
donghquinn <ehdgus1524@gmail.com>
2024-07-27 12:12:43 +0900

    wip: changed queries

--- queries/news/ml.query.go
+++ queries/news/ml.query.go
@@ -5,19 +5,19 @@ var GetTodayMlByDate =  `
 	SELECT *
 	FROM MachineNews
 	WHERE DATE(founded) = $1
+	ORDER BY founded DESC
 	OFFSET $2
 	LIMIT $3
-	ORDER BY founded DESC
-	`
+`
 
 var GetMlByDate =  `
 	SELECT *
 	FROM MachineNews
 	WHERE founded IS BETWEEN $1 AND $2
+	ORDER BY founded DESC
 	OFFSET $3
 	LIMIT $4
-	ORDER BY founded DESC
-	`
+`
 
 
 var LikeMlNews = `
